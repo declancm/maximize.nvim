25e69d3a1a198264280636f9fd3b867ebfbf8678
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-15 20:28:58 -0700

    refactor: Use a lua table instead of tabscoped vim variables

--- lua/maximize/init.lua
+++ lua/maximize/init.lua
@@ -4,6 +4,8 @@ local config = require('maximize.config')
 local integrations = require('maximize.integrations')
 local utils = require('maximize.utils')
 
+local tabscoped = {}
+
 M.setup = function(user_config)
   -- Check if the user has Neovim v0.8.0.
   if vim.fn.has('nvim-0.8.0') == 0 then
@@ -36,7 +38,9 @@ M.maximize = function()
   if #vim.api.nvim_tabpage_list_wins(0) > 1 then
     vim.t.maximized = true
 
-    vim.t._maximize_saved_lazyredraw = vim.o.lazyredraw
+    local tab = vim.api.nvim_get_current_tabpage()
+    tabscoped[tab] = {}
+    tabscoped[tab].lazyredraw = vim.o.lazyredraw
     vim.o.lazyredraw = true
 
     -- Clear the plugin windows.
@@ -62,13 +66,13 @@ M.maximize = function()
     }
 
     -- Prevent session managers from trying to autosave our temporary session
-    vim.t._maximize_old_session = vim.v.this_session
+    tabscoped[tab].session = vim.v.this_session
 
     -- Write the session to a temporary file and save it.
     local tmp_file_name = os.tmpname()
     vim.cmd.mksession({ tmp_file_name, bang = true })
     local tmp_file = assert(io.open(tmp_file_name, 'rb'))
-    vim.t._maximize_saved_session = tmp_file:read('*all')
+    tabscoped[tab].restore_script = tmp_file:read('*all')
     tmp_file:close()
     os.remove(tmp_file_name)
 
@@ -78,15 +82,16 @@ M.maximize = function()
     -- Maximize the window.
     vim.cmd.only()
 
-    vim.o.lazyredraw = vim.t._maximize_saved_lazyredraw
+    vim.o.lazyredraw = tabscoped[tab].lazyredraw
   end
 end
 
 M.restore = function()
   vim.t.maximized = false
 
-  if vim.t._maximize_saved_session then
-    vim.t._maximize_saved_lazyredraw = vim.o.lazyredraw
+  local tab = vim.api.nvim_get_current_tabpage()
+  if tabscoped[tab].restore_script then
+    tabscoped[tab].lazyredraw = vim.o.lazyredraw
     vim.o.lazyredraw = true
 
     -- Save the current buffer and cursor position.
@@ -98,12 +103,10 @@ M.restore = function()
     vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
 
     -- Source the saved session.
-    vim.api.nvim_exec2(vim.t._maximize_saved_session, {})
-    vim.t._maximize_saved_session = nil
+    vim.api.nvim_exec2(tabscoped[tab].restore_script, {})
 
     -- Prevent session managers from trying to autosave our temporary session
-    vim.v.this_session = vim.t._maximize_old_session
-    vim.t._maximize_old_session = nil
+    vim.v.this_session = tabscoped[tab].session
 
     -- Return to previous buffer and cursor position.
     vim.api.nvim_win_set_buf(0, buffer)
@@ -113,7 +116,8 @@ M.restore = function()
     integrations.restore()
     vim.api.nvim_exec_autocmds('User', { pattern = 'WindowRestoreEnd' })
 
-    vim.o.lazyredraw = vim.t._maximize_saved_lazyredraw
+    vim.o.lazyredraw = tabscoped[tab].lazyredraw
+    tabscoped[tab] = {}
   end
 end
 
