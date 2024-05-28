4d4afd984611b65d15e603b111c3ecdfe79f9523
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-27 19:56:06 -0700

    refactor: Shifted the window functions to a windows module

--- lua/maximize/init.lua
+++ lua/maximize/init.lua
@@ -2,8 +2,7 @@ local M = {}
 
 local config = require('maximize.config')
 local integrations = require('maximize.integrations')
-
-local tabscoped = {}
+local windows = require('maximize.windows')
 
 M.setup = function(user_config)
   config.setup(user_config)
@@ -18,15 +17,7 @@ M.setup = function(user_config)
   local group = vim.api.nvim_create_augroup('Maximize', {})
   vim.api.nvim_create_autocmd('WinLeave', {
     group = group,
-    callback = function()
-      if vim.api.nvim_win_get_config(0).relative == '' then
-        local tab = vim.api.nvim_get_current_tabpage()
-        if not tabscoped[tab] then
-          tabscoped[tab] = {}
-        end
-        tabscoped[tab].prev_norm_win = vim.api.nvim_get_current_win()
-      end
-    end,
+    callback = windows.track_last_opened_normal_window,
     desc = 'Keep track of the last non-floating window'
   })
 end
@@ -39,40 +30,9 @@ M.toggle = function()
   end
 end
 
-local leave_floating_window = function()
-  local tab = vim.api.nvim_get_current_tabpage()
-  if vim.api.nvim_win_get_config(0).relative ~= '' then
-    if tabscoped[tab] and tabscoped[tab].prev_norm_win then
-      vim.api.nvim_set_current_win(tabscoped[tab].prev_norm_win)
-    else
-      while vim.api.nvim_win_get_config(0).relative ~= '' do
-        vim.cmd.wincmd('w')
-      end
-    end
-  end
-end
-
-local close_floating_windows = function()
-  for _, window_handle in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if vim.api.nvim_win_get_config(window_handle).relative ~= '' then
-      vim.api.nvim_win_close(window_handle, true)
-    end
-  end
-end
-
 M.maximize = function()
-  local normal_window_count = 0
-  for _, window_handle in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if vim.api.nvim_win_get_config(window_handle).relative == '' then
-      normal_window_count = normal_window_count + 1
-    end
-  end
-
-  if normal_window_count > 1 then
+  if windows.get_normal_window_count() > 1 then
     vim.t.maximized = true
-
-    local tab = vim.api.nvim_get_current_tabpage()
-    tabscoped[tab] = {}
 
     local save_lazyredraw = vim.o.lazyredraw
     vim.o.lazyredraw = true
@@ -80,59 +40,9 @@ M.maximize = function()
     vim.api.nvim_exec_autocmds('User', { pattern = 'WindowMaximizeStart' })
     integrations.clear()
 
-    leave_floating_window()
-    close_floating_windows()
-
-    -- Save the buffer handles, cursor positions and window local variables
-    -- for all windows. Open a temporary scratch buffer in each window.
-    tabscoped[tab].windows = {}
-    local current_window_handle = vim.api.nvim_get_current_win()
-    local windows = vim.api.nvim_tabpage_list_wins(0)
-    for i = 1, #windows do
-      local window = {}
-      window.handle = windows[i]
-      window.save_cursor = vim.api.nvim_win_get_cursor(window.handle)
-      window.save_current_syntax = vim.w[window.handle].current_syntax
-      window.save_quickfix_title = vim.w[window.handle].quickfix_title
-      window.buffer = {}
-      window.buffer.handle = vim.api.nvim_win_get_buf(window.handle)
-      window.buffer.save_bufhidden = vim.bo[window.buffer.handle].bufhidden
-      window.buffer.save_buftype = vim.bo[window.buffer.handle].buftype
-      if window.handle ~= current_window_handle then
-        vim.bo[window.buffer.handle].bufhidden = 'hide'
-        -- Buffer types without associated files aren't restored properly so
-        -- set them to 'nowrite'. Normal buffers can't be closed when modified
-        -- which causes issues when quiting Neovim when maximized.
-        if vim.tbl_contains({ 'quickfix', 'nofile', 'prompt' }, vim.bo[window.buffer.handle].buftype) then
-          vim.bo[window.buffer.handle].buftype = 'nowrite'
-        end
-        vim.api.nvim_win_set_buf(window.handle, vim.api.nvim_create_buf(false, true))
-      end
-      table.insert(tabscoped[tab].windows, window)
-    end
-
-    -- Prevent session managers from trying to autosave our temporary session
-    tabscoped[tab].save_session = vim.v.this_session
-
-    local save_sessionoptions = vim.o.sessionoptions
-    vim.o.sessionoptions = 'blank,help,terminal,winsize'
-
-    -- Write the session to a temporary file and save it.
-    local tmp_file_name = os.tmpname()
-    vim.cmd.mksession({ tmp_file_name, bang = true })
-    local tmp_file = assert(io.open(tmp_file_name, 'rb'))
-    tabscoped[tab].restore_script = tmp_file:read('*all')
-    tmp_file:close()
-    os.remove(tmp_file_name)
-
-    vim.o.sessionoptions = save_sessionoptions
-
-    -- Maximize the window.
-    for _, window in ipairs(tabscoped[tab].windows) do
-      if window.handle ~= current_window_handle then
-        vim.api.nvim_win_close(window.handle, true)
-      end
-    end
+    windows.leave_floating_window()
+    windows.close_floating_windows()
+    windows.maximize_normal_window()
 
     vim.o.lazyredraw = save_lazyredraw
   else
@@ -143,57 +53,18 @@ end
 M.restore = function()
   vim.t.maximized = false
 
-  local tab = vim.api.nvim_get_current_tabpage()
-  if tabscoped[tab] and tabscoped[tab].restore_script then
+  if windows.normal_windows_restorable() then
     local save_lazyredraw = vim.o.lazyredraw
     vim.o.lazyredraw = true
 
-    leave_floating_window()
-    close_floating_windows()
-
-    local save_buffer = vim.api.nvim_get_current_buf()
-    local save_cursor = vim.api.nvim_win_get_cursor(0)
-    local save_bufhidden = vim.bo.bufhidden
-    vim.bo.bufhidden = 'hide'
-
-    local save_eventignore = vim.o.eventignore
-    vim.opt.eventignore:append('SessionLoadPost')
-
-    -- The current buffer when sourcing a session can't be
-    -- modified so create and open a temporary unlisted buffer.
-    vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
-    _ = vim.api.nvim_exec2(tabscoped[tab].restore_script, { output = true })
-
-    -- Prevent session managers from trying to autosave our temporary session
-    vim.v.this_session = tabscoped[tab].save_session
-    vim.o.eventignore = save_eventignore
-
-    vim.api.nvim_win_set_buf(0, save_buffer)
-    vim.api.nvim_win_set_cursor(0, save_cursor)
-    vim.bo.bufhidden = save_bufhidden
-
-    -- Restore the buffer handles, cursor positions and
-    -- window local variables for all restored windows.
-    local current_window_handle = vim.api.nvim_get_current_win()
-    local windows = vim.api.nvim_tabpage_list_wins(0)
-    for i = 1, #windows do
-      local window = tabscoped[tab].windows[i]
-      local window_handle = windows[i]
-      if window_handle ~= current_window_handle then
-        vim.api.nvim_win_set_buf(window_handle, window.buffer.handle)
-        vim.api.nvim_win_set_cursor(window_handle, window.save_cursor)
-        vim.bo[window.buffer.handle].bufhidden = window.buffer.save_bufhidden
-        vim.w[window_handle].current_syntax = vim.w[window_handle].current_syntax or window.save_current_syntax
-        vim.w[window_handle].quickfix_title = vim.w[window_handle].quickfix_title or window.save_quickfix_title
-      end
-    end
+    windows.leave_floating_window()
+    windows.close_floating_windows()
+    windows.restore_normal_windows()
 
     integrations.restore()
     vim.api.nvim_exec_autocmds('User', { pattern = 'WindowRestoreEnd' })
 
     vim.o.lazyredraw = save_lazyredraw
-
-    tabscoped[tab] = {}
   end
 end
 
---
+++ lua/maximize/windows.lua
@@ -0,0 +1,161 @@
+local M = {}
+
+local tabscoped = {}
+
+local is_floating_window = function(window_handle)
+  return vim.api.nvim_win_get_config(window_handle).relative ~= ''
+end
+
+M.get_normal_window_count = function()
+  local count = 0
+  for _, window_handle in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if not is_floating_window(window_handle) then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+M.clear_tabscoped_variables = function()
+  tabscoped[vim.api.nvim_get_current_tabpage()] = {}
+end
+
+M.track_last_opened_normal_window = function()
+  if not is_floating_window(0) then
+    local tab = vim.api.nvim_get_current_tabpage()
+    tabscoped[tab] = tabscoped[tab] or {}
+    tabscoped[tab].last_norm_win = vim.api.nvim_get_current_win()
+  end
+end
+
+M.leave_floating_window = function()
+  local tab = vim.api.nvim_get_current_tabpage()
+  tabscoped[tab] = tabscoped[tab] or {}
+
+  if is_floating_window(0) then
+    if tabscoped[tab] and tabscoped[tab].last_norm_win then
+      vim.api.nvim_set_current_win(tabscoped[tab].last_norm_win)
+    else
+      while is_floating_window(0) do
+        vim.cmd.wincmd('w')
+      end
+    end
+  end
+end
+
+M.close_floating_windows = function()
+  for _, window_handle in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if is_floating_window(window_handle) then
+      vim.api.nvim_win_close(window_handle, true)
+    end
+  end
+end
+
+M.maximize_normal_window = function()
+  local tab = vim.api.nvim_get_current_tabpage()
+  tabscoped[tab] = tabscoped[tab] or {}
+
+  -- Save the buffer handles, cursor positions and window local variables
+  -- for all windows. Open a temporary scratch buffer in each window.
+  tabscoped[tab].windows = {}
+  local current_window_handle = vim.api.nvim_get_current_win()
+  local windows = vim.api.nvim_tabpage_list_wins(0)
+  for i = 1, #windows do
+    local window = {}
+    window.handle = windows[i]
+    window.save_cursor = vim.api.nvim_win_get_cursor(window.handle)
+    window.save_current_syntax = vim.w[window.handle].current_syntax
+    window.save_quickfix_title = vim.w[window.handle].quickfix_title
+    window.buffer = {}
+    window.buffer.handle = vim.api.nvim_win_get_buf(window.handle)
+    window.buffer.save_bufhidden = vim.bo[window.buffer.handle].bufhidden
+    window.buffer.save_buftype = vim.bo[window.buffer.handle].buftype
+    if window.handle ~= current_window_handle then
+      vim.bo[window.buffer.handle].bufhidden = 'hide'
+      -- Buffer types without associated files aren't restored properly so
+      -- set them to 'nowrite'. Normal buffers can't be closed when modified
+      -- which causes issues when quiting Neovim when maximized.
+      if vim.tbl_contains({ 'quickfix', 'nofile', 'prompt' }, vim.bo[window.buffer.handle].buftype) then
+        vim.bo[window.buffer.handle].buftype = 'nowrite'
+      end
+      vim.api.nvim_win_set_buf(window.handle, vim.api.nvim_create_buf(false, true))
+    end
+    table.insert(tabscoped[tab].windows, window)
+  end
+
+  -- Prevent session managers from trying to autosave our temporary session
+  tabscoped[tab].save_session = vim.v.this_session
+
+  local save_sessionoptions = vim.o.sessionoptions
+  vim.o.sessionoptions = 'blank,help,terminal,winsize'
+
+  -- Write the session to a temporary file and save it.
+  local tmp_file_name = os.tmpname()
+  vim.cmd.mksession({ tmp_file_name, bang = true })
+  local tmp_file = assert(io.open(tmp_file_name, 'rb'))
+  tabscoped[tab].restore_script = tmp_file:read('*all')
+  tmp_file:close()
+  os.remove(tmp_file_name)
+
+  vim.o.sessionoptions = save_sessionoptions
+
+  -- Maximize the window.
+  for _, window in ipairs(tabscoped[tab].windows) do
+    if window.handle ~= current_window_handle then
+      vim.api.nvim_win_close(window.handle, true)
+    end
+  end
+end
+
+M.normal_windows_restorable = function()
+  local tab = vim.api.nvim_get_current_tabpage()
+  tabscoped[tab] = tabscoped[tab] or {}
+
+  return tabscoped[tab].restore_script ~= nil
+end
+
+M.restore_normal_windows = function()
+  local tab = vim.api.nvim_get_current_tabpage()
+  tabscoped[tab] = tabscoped[tab] or {}
+
+  local save_buffer = vim.api.nvim_get_current_buf()
+  local save_cursor = vim.api.nvim_win_get_cursor(0)
+  local save_bufhidden = vim.bo.bufhidden
+  vim.bo.bufhidden = 'hide'
+
+  local save_eventignore = vim.o.eventignore
+  vim.opt.eventignore:append('SessionLoadPost')
+
+  -- The current buffer when sourcing a session can't be
+  -- modified so create and open a temporary unlisted buffer.
+  vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
+  _ = vim.api.nvim_exec2(tabscoped[tab].restore_script, { output = true })
+
+  -- Prevent session managers from trying to autosave our temporary session
+  vim.v.this_session = tabscoped[tab].save_session
+  vim.o.eventignore = save_eventignore
+
+  vim.api.nvim_win_set_buf(0, save_buffer)
+  vim.api.nvim_win_set_cursor(0, save_cursor)
+  vim.bo.bufhidden = save_bufhidden
+
+  -- Restore the buffer handles, cursor positions and
+  -- window local variables for all restored windows.
+  local current_window_handle = vim.api.nvim_get_current_win()
+  local windows = vim.api.nvim_tabpage_list_wins(0)
+  for i = 1, #windows do
+    local window = tabscoped[tab].windows[i]
+    local window_handle = windows[i]
+    if window_handle ~= current_window_handle then
+      vim.api.nvim_win_set_buf(window_handle, window.buffer.handle)
+      vim.api.nvim_win_set_cursor(window_handle, window.save_cursor)
+      vim.bo[window.buffer.handle].bufhidden = window.buffer.save_bufhidden
+      vim.w[window_handle].current_syntax = vim.w[window_handle].current_syntax or window.save_current_syntax
+      vim.w[window_handle].quickfix_title = vim.w[window_handle].quickfix_title or window.save_quickfix_title
+    end
+  end
+
+  tabscoped[tab] = {}
+end
+
+return M
